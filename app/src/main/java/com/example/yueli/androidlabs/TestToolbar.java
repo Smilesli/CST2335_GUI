1f558f6490a1dc48f6f78f926ccc70d062050bc3
Yueli Zhu <zhu00117@algonquinlive.com>
2018-11-26 00:53:34 -0500

    step 10

--- app/src/main/java/com/example/yueli/androidlabs/TestToolbar.java
+++ app/src/main/java/com/example/yueli/androidlabs/TestToolbar.java
@@ -1,5 +1,9 @@
 package com.example.yueli.androidlabs;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -36,12 +40,40 @@ public class TestToolbar extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         switch(item.getItemId()){
             case R.id.action_one:
-                Log.i(TAG, "iteam one is selected");
-                break;
-            case R.id.action_two:
+            {
+                String currentMessage = "You selected item 1";
+                Snackbar.make(findViewById(R.id.action_one),currentMessage,Snackbar.LENGTH_SHORT).show();
+                //Log.i(TAG, "iteam one is selected");
+            }
+            break;
+
+
+            case R.id.action_two:{
+                //AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                AlertDialog.Builder builder = new AlertDialog.Builder(TestToolbar.this);
+                builder.setTitle(R.string.dialog_title_toolbar)
+                        .setNegativeButton(R.string.cancel,new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int id){
+
+                            }
+                        })
+                        .setPositiveButton(R.string.ok,new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int id){
+                                //Intent resultIntent = new Intent();
+                                //resultIntent.putExtra("Response","Here is my response");
+                                //setResult(Activity.RESULT_OK,resultIntent);
+                                finish();
+                            }
+                        });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
                 Log.i(TAG, "iteam two is selected");
                 break;
             case R.id.action_three:
+            {
+            }
                 Log.i(TAG, "iteam three is selected");
                 break;
             case R.id.action_4:
@@ -50,6 +82,7 @@ public class TestToolbar extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 
     public void onButtonClick(View view){
         Snackbar.make((Button)findViewById(R.id.button5),"Message to show",Snackbar.LENGTH_SHORT).show();
